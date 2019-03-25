f3884f06ece8aa76eb11b982ac4c9a8935fbe3f3
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-25 15:30:14 +0100

    Burger component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import classes from './Burger.module.css'
+import BurgerIngredient from './BurgerIngredient/BurgerIngriedient'
+
+const Burger = props => {
+    return (
+        <div
+            className={classes.Burger}
+        >
+            <BurgerIngredient
+                type="bread-top"
+            />
+            <BurgerIngredient
+                type="cheese"
+            />
+            <BurgerIngredient
+                type="meat"
+            />
+            <BurgerIngredient
+                type="bread-bottom"
+            />
+        </div>
+    )
+}
+
+export default Burger
