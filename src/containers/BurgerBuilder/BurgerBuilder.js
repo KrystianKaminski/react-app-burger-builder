e912a3329f2da0c97c495ac74d017d5b29fbba1c
KrystianKaminski <krystiankaminski07@gmail.com>
2019-03-30 15:08:09 +0100

    Navigation to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,6 +28,7 @@ class BurgerBuilder extends React.Component {
     }
 
     componentDidMount() {
+        console.log(this.props)
         axios.get('https://react-my-burger-edeb5.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({
@@ -96,34 +97,35 @@ class BurgerBuilder extends React.Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({ loading: true })
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Krystian',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '12312',
-                    country: 'Poland'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                })
-            })
-            .catch(error => {
-                this.setState({
-                    loading: false,
-                    purchasing: false
-                })
-            })
+        // this.setState({ loading: true })
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Krystian',
+        //         address: {
+        //             street: 'Teststreet 1',
+        //             zipCode: '12312',
+        //             country: 'Poland'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         })
+        //     })
+        //     .catch(error => {
+        //         this.setState({
+        //             loading: false,
+        //             purchasing: false
+        //         })
+        //     })
+        this.props.history.push('/checkout')
     }
 
     render() {
