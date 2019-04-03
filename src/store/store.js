7a6793626d9cdf53d7a895258a48f15371245a5c
KrystianKaminski <krystiankaminski07@gmail.com>
2019-04-03 13:54:37 +0200

    Combining reducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import burgerBuilderReducer from './reducers/burgerBuilder'
+import orderReducer from './reducers/order'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(burgerBuilderReducer, composeEnhancers(
+const rootReducer = combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer
+})
+
+const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk)
 ))
 
