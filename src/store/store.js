f4b5e906f2db599ed916ed861ad288c1970258a0
KrystianKaminski <krystiankaminski07@gmail.com>
2019-04-04 14:40:13 +0200

    Storing the token from firebase

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import burgerBuilderReducer from './reducers/burgerBuilder'
 import orderReducer from './reducers/order'
+import authReducer from './reducers/auth'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
-    order: orderReducer
+    order: orderReducer,
+    auth: authReducer
 })
 
 const store = createStore(rootReducer, composeEnhancers(
